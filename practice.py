a8cd88433c13d13d5956379cb968e9a8ee5d66d1
vvwwvw <minhoon1101@gmail.com>
2021-03-19 15:52:51 +0900

    mygit

---
+++ practice.py
@@ -0,0 +1,45 @@
+# from random import *
+
+# # 방법1
+# print("나는 %d살입니다." % 20)
+# print("나는 %s살입니다." % 20)
+# print("나는 %s을 좋아해요" % "파이썬")
+# print("Apple %c로 시작해요" % "A")
+
+
+# # 방법2
+# print("나는{}살입니다." .format(20))
+
+
+
+# print("나는 {}살이며" .format("f"))
+
+
+# age = 20
+# color = "red"
+
+# print(f"나는{age}")
+
+
+
+# cabinet = {1:"a", 2:"b"}
+# print(cabinet[1])
+
+# cabinet[3] = "c"
+# print(cabinet)
+# cabinet[1] = "aa"
+# print(cabinet)
+
+# del cabinet[1]
+# print(cabinet)
+# print(cabinet.keys())
+# print(cabinet.values())
+# print(cabinet.items())
+
+# cabinet.clear()
+# print(cabinet.items())
+
+menu = ("a","b")
+
+print(menu[0])
+print(menu[1])
